1b8094c8a49665c270df1db0460f0c77362b935f
Peter Hartmann <github@peterh.33mail.com>
2019-05-03 10:34:07 +0200

    simplify example

--- sample-project/main.cpp
+++ sample-project/main.cpp
@@ -1,11 +1,8 @@
 #include <QtCore>
-#include <QtQuick>
 
-int main(int argc, char **argv)
+int main(int /*argc*/, char **/*argv*/)
 {
-    QGuiApplication guiApp(argc, argv);
-    auto engine = new QQmlEngine;
-    engine = nullptr;
-    qDebug() << engine->baseUrl();
+    QStringList list = {"one", "two"};
+    qDebug() << list[2];
 }
 
